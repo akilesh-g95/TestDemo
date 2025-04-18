e543b8d4f654f25d0b58a8fa4bf5b50c1d4b993f
Akilesh G <akhilyakshaa@gmail.com>
2025-04-18 12:54:49 +0530

    Added new class with reverse string

---
+++ src/cheating/reversestring.java
@@ -0,0 +1,23 @@
+package cheating;
+
+import java.util.Scanner;
+
+public class reversestring {
+
+	public static void main(String[] args) {
+	
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the name which needs to be reversed");
+		String normalstring = sc.nextLine();
+		sc.close();
+	char[] ch = normalstring.toCharArray();
+
+	String rev = "" ;
+	
+	for(int i = ch.length-1; i>=0; i--) {
+		 rev = rev+ch[i];
+	}
+	System.out.println(rev);
+	}
+
+}
